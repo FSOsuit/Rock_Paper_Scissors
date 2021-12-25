190cd7c2f5f8384ebe1ef36fe83b9aa98b43e7de
FSOsuit <druzhba2@inbox.lv>
2021-12-25 14:10:21 +0200

    added function playRound

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 function computerPlay() {
-    const compOptions = ["Rock", "Paper", "Scissors"];
+    const compOptions = ["rock", "paper", "scissors"];
     let randomNum = Math.floor(Math.random() * compOptions.length);
     return compOptions[randomNum];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    let playerMove = playerSelection.toLowerCase();
 
+    if (playerMove === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors.";
+    }else if (playerMove === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beat paper";
+    } else if (playerMove === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock.";
+    } else if (playerMove === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock."
+    } else if (playerMove === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors."
+    } else if (playerMove === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper."
+    } else {
+        return "It's a Draw!"
+    }
+
+
+
+    
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
